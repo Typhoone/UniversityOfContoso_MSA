19a0409a5b2b72959b6a1b440c97a638f347ac56
Gordon Adams <gordonadamsnz@gmail.com>
2015-11-20 15:13:43 +1300

    Added Models for Constructor Added Facebook API

---
+++ UniversityOfContoso/UniversityOfContoso/Models/Assignment.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniversityOfContoso.Models
+{
+    class Assignment
+    {
+        public string AssignmentID { get; set; }
+        public string CourseID { get; set; }
+        public DateTime DueTime { get; set; }
+        public string AssigLink { get; set; }
+    }
+}
---
+++ UniversityOfContoso/UniversityOfContoso/Models/Enrollment.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniversityOfContoso.Models
+{
+    class Enrollment
+    {
+        public int EnrollmentID { get; set; }
+        public int CourseID { get; set; }
+        public int studentID { get; set; }
+        public int gradePercent { get; set; }
+        public ICollection<string> submisions { get; set; }
+
+        public Student Student { get; set; }
+
+        public Course Course { get; set; }
+
+    }
+}
