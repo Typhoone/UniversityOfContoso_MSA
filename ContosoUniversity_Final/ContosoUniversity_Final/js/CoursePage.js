6a45661a958bbfaee4be9c073d77a81314db96f0
Gordon Adams <gordonadamsnz@gmail.com>
2015-11-23 09:55:47 +1300

    Javascript added
    Added java script to obtain infomation from the databasse

---
+++ ContosoUniversity_Final/ContosoUniversity_Final/js/CoursePage.js
@@ -0,0 +1,37 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+    var controller = document.body.getAttribute("data-controller");
+    if (controller == "courses") {
+        loadCoursesTable(controller);
+    }
+});
+
+function loadCoursesTable(controller) {
+
+    var courseTable = document.getElementById("courseContentTable");
+
+    enrollmentModule.getEnrollment(function (rollList) {
+        setupCourseTable(rollList);
+    });
+
+    function setupCourseTable(enrollments) {
+
+        var userID = 1;
+
+        //traverses thorugh enrollemts to find a math, poor code but best i can do at this stage
+        for (i = 0; i < enrollments.length; i++) {
+            if (enrollments[i].studentID == userID) {
+                courseModule.getCoursefromID(enrollments[i].CourseID, function (courseList) {//making function in loop even worse code but i dont know a better way
+                    displayCourse(courseList);
+                });
+            }
+        }
+
+        //appends table with data
+        function displayCourse(courses){
+
+        }
+
+
+    }
+
+}
---
+++ ContosoUniversity_Final/ContosoUniversity_Final/js/EnrollmentsModule.js
@@ -0,0 +1,29 @@
+﻿var enrollmentModule = (function () {
+
+    return {
+        getEnrollment: function (id, callback) {
+            var xhttp = new XMLHttpRequest();
+
+            xhttp.onreadystatechange = function () {
+                //this is called after the xhhtp.send as at this stage the xhttp var has not been changed
+                if (xhttp.readyState == 4 && xhttp.status == 200) {
+                    loadedRollInfo();
+                }
+
+                //connects and queries for the data
+                xhttp.open("GET", "http://contosouniversitygmadams.azurewebsites.net/api/Enrollments", true);
+                xhttp.setRequestHeader("Content-type", "application/json");
+                xhttp.send(null);
+
+
+                //parse the JSON data and send it back to that who called it
+                function loadedRollInfo() {
+                    var rollList = JSON.parse(xhttp.responseText);
+                    callback(rollList);
+                    return rollList;
+                }
+            };
+        }
+    };
+
+}());
